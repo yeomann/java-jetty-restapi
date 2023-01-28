5c6c5711fc29582faed49176d83ded89965ed79b
yeomann <idanishraza@gmail.com>
2023-01-28 22:36:42 +0300

    added slf4j api and logback for logging

--- my-app-server/src/main/java/com/mycompany/server/ApiServer.java
+++ my-app-server/src/main/java/com/mycompany/server/ApiServer.java
@@ -1,12 +1,15 @@
 package com.mycompany.server;
 
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.eclipse.jetty.http.HttpScheme.HTTP;
 
 public class ApiServer {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ApiServer.class);
   public static void main(String... args) {
-    System.out.println("Hello world!");
+    LOGGER.info("Hello world!");
 
     HttpConfiguration httpsConfiguration = new HttpConfiguration();
     httpsConfiguration.setSecureScheme(HTTP.asString());
